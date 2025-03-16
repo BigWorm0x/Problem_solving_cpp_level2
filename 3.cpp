28cd7dbe409446af4d669aa8dd999b6b374b76f9
BigWorm0x <101954335+BigWorm0x@users.noreply.github.com>
2025-03-16 23:13:06 +0200

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -50,3 +50,64 @@ int main()
     bool checkNum = checkNumberIsPerfectOrNot(read);
     printResult(checkNum);
 }
+
+
+
+// with enum 
+
+#include <iostream>
+using namespace std;
+
+enum ePerfectNumber { perfect = 1, notPerfect = 2 };
+
+int readNumber()
+{
+    int number;
+    do
+    {
+        cout << "Enter number to check if this number is perfect or not: \n";
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+
+ePerfectNumber checkNumberPerfectOrNot(int number)
+{
+    int perfectSum = 0;
+    for (int i = 1; i < number; i++)  
+    {
+        if (number % i == 0)
+        {
+            perfectSum += i;
+        }
+    }
+
+    if (perfectSum == number)  
+    {
+        return ePerfectNumber::perfect;
+    }
+    else
+    {
+        return ePerfectNumber::notPerfect;
+    }
+}
+
+void printResult(ePerfectNumber result)
+{
+    if (result == ePerfectNumber::perfect)  
+    {
+        cout << "This is a perfect number.\n";
+    }
+    else
+    {
+        cout << "This is not a perfect number.\n";
+    }
+}
+
+int main()
+{
+    int number = readNumber();
+    ePerfectNumber result = checkNumberPerfectOrNot(number);
+    printResult(result);
+    return 0;
+}
