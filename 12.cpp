f7d44eda7a515655ccd5024a1de4c4b7d3776550
BigWorm0x <101954335+BigWorm0x@users.noreply.github.com>
2025-03-21 21:09:23 +0200

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <limits>
+using namespace std;
+
+int readPositiveNumber(string message)
+{
+    long number;
+    while (true)
+    {
+        cout << message;
+        cin >> number;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a valid number.\n";
+        }
+        else if (number < 0)
+        {
+            cout << "Please enter a positive number.\n";
+        }
+        else
+        {
+            return number;
+        }
+    }
+}
+
+
+void printResult(int number)
+{
+    for (int i = number; i >= 1; i--)
+    {
+        for (int j = 1 ; j <= i ; j++ )
+        {
+            cout << i;
+        }
+        cout << "\n";
+    }
+}
+
+
+int main()
+{
+
+    printResult(readPositiveNumber("enter a number: \n"));
+}
