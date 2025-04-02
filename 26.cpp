752002221ef6b92cd2c6db06c1ba06f3eeabdc53
BigWorm0x <101954335+BigWorm0x@users.noreply.github.com>
2025-04-02 18:02:33 +0200

    Create 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <ctime> 
+
+using namespace std;
+
+int readPositiveNumber(string msg) {
+    int number;
+    while (true) {
+        cout << msg;
+        cin >> number;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please enter a number: ";
+        }
+        else if (number <= 0) {
+            cout << "Please enter a positive number: ";
+        }
+        else if (number > 100) {
+            cout << "The maximum allowed number is 100. Please enter a valid number: ";
+        }
+        else {
+            return number;
+        }
+    }
+}
+
+int randNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+vector<int> generateRandomArray(int number) {
+    vector<int> arrayOfRandomNumbers(number);
+    for (int& num : arrayOfRandomNumbers) {
+        num = randNumber(1, 101);
+    }
+    return arrayOfRandomNumbers;
+}
+
+void printArrayElements(const vector<int>& arr) {
+    cout << "This is the array elements: \n";
+
+    int sum = 0; 
+    for (size_t i = 0; i < arr.size(); i++) {
+        cout << "[" << i + 1 << "] " << arr[i] << "\n";
+        sum = sum + arr[i];
+    }
+
+    cout << "sum aof all numbers in array = " << sum << endl;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int read = readPositiveNumber("Please enter the number of array elements (max 100): ");
+    vector<int> randomNumbers = generateRandomArray(read);
+
+    printArrayElements(randomNumbers);
+
+    return 0;
+}
